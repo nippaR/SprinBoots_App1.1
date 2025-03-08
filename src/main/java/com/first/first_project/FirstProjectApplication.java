45475f739d4e6ea16095592e8525a7d522e45e17
Thanuja Nipun <thanujanipun6@gmail.com>
2025-03-08 22:47:42 +0530

    Labsheet 03 done

--- src/main/java/com/first/first_project/FirstProjectApplication.java
+++ src/main/java/com/first/first_project/FirstProjectApplication.java
@@ -2,12 +2,30 @@ package com.first.first_project;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+
 
 @SpringBootApplication
+@RestController
 public class FirstProjectApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(FirstProjectApplication.class, args);
 	}
 
+	@GetMapping("/")
+	public String rootEndPoint() {
+		String message = "Hello World!";
+		return message;
+	}
+	
+	@GetMapping("/hello")
+	public String helloEndPoint(@RequestParam(value = "name", defaultValue = "World") String name) {
+		String message = "Hello " + name + "!";
+		return message;
+	}
+	
 }
